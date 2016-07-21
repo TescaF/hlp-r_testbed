fc4672a5f31ed3adb7550b47445d74b3d337b6da
TescaF <tesca.fitzgerald@gmail.com>
2016-07-21 18:06:15 -0500

    added self-collision checking with moveit

--- manipulation_states_simple.py
+++ manipulation_states_simple.py
@@ -5,6 +5,7 @@ import rospy
 import smach
 import smach_ros
 import tf
+import signal
 import sys
 import time
 
@@ -33,6 +34,7 @@ def initGlobals():
   global _isTheManipulationStateGlobalsInitialized
   global _traDict
   global _ptNum
+  global _group
 
   _ptNum = 0
   _arm_planner = ArmMoveIt()
@@ -47,23 +49,66 @@ def initGlobals():
                 [-1.60, 2.10, 1.00, -2.50, 1.50, 1.20],
                 [-1.60, 2.20, 1.20, -3.14, 1.50, 1.20]]
 
-  eeTra =      [[0.554,0.039,1.380,0.235,0.799,0.551],
-		[0.765,0.091,1.398,0.455,0.766,0.428],
-		[0.826,0.129,1.373,0.663,0.669,0.289],
-		[0.812,0.139,1.293,0.768,0.597,0.052],
-		[0.915,0.146,1.302,0.852,0.471,0.092],
-		[1.047,0.119,1.349,0.966,0.190,0.137]]
+  eeTra =      [[0.554,0.039,1.580,0.235,0.799,0.551],
+		[0.765,0.091,1.598,0.455,0.766,0.428],
+		[0.826,0.129,1.573,0.663,0.669,0.289],
+		[0.812,0.139,1.493,0.768,0.597,0.052],
+		[0.915,0.146,1.502,0.852,0.471,0.092],
+		[1.047,0.119,1.549,0.966,0.190,0.137]]
 
   resetPos = [[-1.90, 1.50, 0.50, -2.00, 3.00, 0.72]]
 
   _traDict = {'person':handOffTra,'reset':resetPos, 'ee':eeTra}
 
+  moveit_commander.roscpp_initialize(sys.argv)
+  _robot = moveit_commander.RobotCommander()
+  _scene = moveit_commander.PlanningSceneInterface()
+  _group = [moveit_commander.MoveGroupCommander("arm"), moveit_commander.MoveGroupCommander("gripper"),
+         moveit_commander.MoveGroupCommander("head")]
+
+def sigint_handler(signum, frame):
+  sys.exit(1)
+
+signal.signal(signal.SIGINT, sigint_handler)
+
 def sendPlan(arm, plannedTra):
   for pt in plannedTra:
-    print "goto: " + str(pt) 
     arm.ang_pos_cmd(pt)
+    #time.sleep(0.1)
+  return
+
+def sendPlanTrajectory(arm, plannedTra):
+  for pt in plannedTra:
+    arm.ang_pos_cmd(pt.positions)
     time.sleep(0.1)
   return
+
+def sendPlanNew(arm, plannedTra):
+  newPlan = []
+  for pt in plannedTra:
+    print "goto: " + str(pt) 
+    #try:
+    _group[0].set_joint_value_target(pt)
+    subplan = _group[0].plan().joint_trajectory.points
+    for pt2 in subplan:
+      newPlan.append(pt2.positions)
+    #except Exception:
+    #  print "Point out-of-bounds"
+  for pt in newPlan:
+    arm.ang_pos_cmd(pt)
+    #time.sleep(1.0)
+  return
+
+def sendEEPlan (arm, plannedTra):
+  waypoints = []
+  for pt in plannedTra:
+    _group[0].set_position_target(pt)
+    subplan = _group[0].plan().joint_trajectory.points
+    for pt2 in subplan:
+      waypoints.append(pt2.positions)
+  for pt in waypoints:
+    arm.ang_pos_cmd(pt)
+    time.sleep(0.1)
 
 class ExecuteTrajectoryState(smach.State):
   def __init__(self):
@@ -97,7 +142,8 @@ class ExecuteTrajectoryState(smach.State):
       return 'succeeded'
     else:
       print 'Executing trajectory'
-      sendPlan(self.arm, userdata.trajectoryIn)
+      sendPlanNew(self.arm, userdata.trajectoryIn)
+      #sendEEPlan(self.arm, userdata.trajectoryIn)
       self.ss.say("Now executing the arm trajectory")
       userdata.execResultOut = 'done'
       return 'succeeded'
@@ -177,7 +223,7 @@ class PlanEEDMPState(smach.State):
     goal_thresh = [0.2,0.2,0.2,0.2,0.2,0.2]
     seg_length = -1          #Plan until convergence to goal
     tau = 2 * req.tau       #Desired plan should take twice as long as demo
-    dt = 0.1
+    dt = 1.0
     integrate_iter = 1  
     
     ee_x_0 = self.transform(x_0,"/base_link","/linear_actuator_link")
@@ -190,6 +236,10 @@ class PlanEEDMPState(smach.State):
     print 'ee plan'
     for pt in cvt_plan:
       print pt
+
+    #userdata.traOut = cvt_plan
+    #return 'succeeded'
+
     js_plan = []
     prev = _traDict['person'][0] #js_x_0
     print 'js plan'
