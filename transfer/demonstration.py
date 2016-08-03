0fb5cc3baedbf4e8ec585d58a43b3f3414da34aa
TescaF <tesca.fitzgerald@gmail.com>
2016-08-03 12:36:42 -0500

    fixed runtime errors

--- transfer/demonstration.py
+++ transfer/demonstration.py
@@ -9,7 +9,8 @@ import time
 import actionlib
 import rosbag
 
-from hlpr_kinesthetic_teaching.kinesthetic_interaction import *
+from hlpr_kinesthetic_teaching.kinesthetic_interaction import KinestheticInteraction 
+from hlpr_kinesthetic_teaching.srv import KinestheticInteract
 from hlpr_kinesthetic_teaching.msg import RecordKeyframeDemoAction, RecordKeyframeDemoGoal, RecordKeyframeDemoResult, RecordKeyframeDemoFeedback
 from std_msgs.msg import Int32, String
 
@@ -21,12 +22,23 @@ def initGlobals():
 class Demonstration(KinestheticInteraction):
   def __init__(self):
     # Initialize the node
-    super(BasicKinestheticInteraction, self).__init__()
-    self.filename = "demo_data"
+    super(Demonstration, self).__init__()
+    self.setup()
+    rospy.spin()
+
+  def setup(self):
+    self.filename = "/home/tesca/data/demos/demo_traj"
+    self.finished = False
+
+    #start demo recording
     self.pub = rospy.Publisher('record_demo_frame', Int32, queue_size=10)
-    client = actionlib.SimpleActionClient('record_keyframe_demo', RecordKeyframeDemoAction)
-    client.wait_for_server()
-    self.finished = false
+    self.client = actionlib.SimpleActionClient('record_keyframe_demo', RecordKeyframeDemoAction)
+    self.client.wait_for_server()
+
+    #start kinesthetic mode
+    rospy.wait_for_service('kinesthetic_interaction')
+    self.teachingMode = rospy.ServiceProxy('kinesthetic_interaction', KinestheticInteract)
+    self.teachingMode(True)
 
   def feedback_call(feedback):
     print('[Feedback] num keyframes: %f'%feedback.num_keyframes)
@@ -41,9 +53,10 @@ class Demonstration(KinestheticInteraction):
     #start next trajectory
     print "start"
     self.trajNum = 0
-    goal.bag_file_name = filename + "_" + str(self.trajNum)
-    client.send_goal(goal, feedback_cb=feedback_call)
-    pub.publish(0)
+    goal = RecordKeyframeDemoGoal()
+    goal.bag_file_name = self.filename + "_" + str(self.trajNum)
+    self.client.send_goal(goal, feedback_cb=self.feedback_call)
+    self.pub.publish(0)
 
   def demonstration_keyframe(self, cmd):
     print "keyframe"
@@ -51,27 +64,30 @@ class Demonstration(KinestheticInteraction):
   def demonstration_end(self, cmd):
     #end current trajectory
     print "end"
-    pub.publish(3)
-    client.wait_for_result()
-    print('[Result] State: %d'%(client.get_state()))
-    print('[Result] Status: %s'%(client.get_goal_status_text()))
-    print('[Result] Num Keyframes Recorded: %f'%(client.get_result().num_keyframes))
-    self.finished = true
+    self.pub.publish(3)
+    self.client.wait_for_result()
+    print('[Result] State: %d'%(self.client.get_state()))
+    print('[Result] Status: %s'%(self.client.get_goal_status_text()))
+    print('[Result] Num Poses Recorded: %f'%(self.client.get_result().num_keyframes))
+    self.teachingMode(False)
+    self.finished = True
 
   def demonstration_start_trajectory(self, cmd):
     #end current trajectory
+    print "next trajectory"
     print cmd
-    pub.publish(3)
-    client.wait_for_result()
-    print('[Result] State: %d'%(client.get_state()))
-    print('[Result] Status: %s'%(client.get_goal_status_text()))
-    print('[Result] Num Keyframes Recorded: %f'%(client.get_result().num_keyframes))
+    self.pub.publish(3)
+    self.client.wait_for_result()
+    print('[Result] State: %d'%(self.client.get_state()))
+    print('[Result] Status: %s'%(self.client.get_goal_status_text()))
+    print('[Result] Num Keyframes Recorded: %f'%(self.client.get_result().num_keyframes))
 
     #start next trajectory
     self.trajNum += 1
-    goal.bag_file_name = filename + "_" + str(self.trajNum)
-    client.send_goal(goal, feedback_cb=feedback_call)
-    pub.publish(0)
+    goal = RecordKeyframeDemoGoal()
+    goal.bag_file_name = self.filename + "_" + str(self.trajNum)
+    self.client.send_goal(goal, feedback_cb=self.feedback_call)
+    self.pub.publish(0)
 
   def demonstration_end_trajectory(self, cmd):
     print cmd
@@ -85,9 +101,12 @@ class DemonstratonState(smach.State):
                          output_keys=['statusOut'])
     if not _initialized:
       initGlobals()
+    self.teachingMode = rospy.ServiceProxy('kinesthetic_interaction', KinestheticInteract)
 
   def execute(self, userdata):
     demo = Demonstration()
+    while not demo.finished:
+      time.sleep(0.1)
     
     userdata.statusOut = 'succeeded'
     return 'succeeded'
